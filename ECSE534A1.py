4c9a291520a8e4b0c5cfb404101298578ad6f27d
Dirk7589 <dirk.a.dubois@gmail.com>
2016-09-19 19:28:45 -0400

    Added method that generates the test data based on an upper and lower bound.

--- ECSE534A1.py
+++ ECSE534A1.py
@@ -1,17 +1,34 @@
 import numpy as np
 import random
 
+def createTestMatrices(lowerBound, upperBound):
+    """Computes a series of test matrices for Choleski algorithm
+    :param lowerBound: dimension of smallest test matrix
+    :param upperBound: dimension of largest test matrix
+    Return the test matrices
+    """
+    if lowerBound <= 1:
+        raise IndexError('lowerBound must be larger than 1')
+    elif upperBound <= lowerBound:
+        raise IndexError('upperBound must be larger than lowerBound')
+    testMatrices = []
+    for i in range(lowerBound, upperBound +1):
+        testMatrices.append(createTestMatrix(i))
+    return testMatrices
+
 def createTestMatrix(size):
-    #add size check
-    testMatrix = None
-    aMatrix = np.zeros((size,size))
-    for row in np.nditer(aMatrix, op_flags=['readwrite']):
-        row[...] = random.randrange(1, 9)
+    """Creates an nxn matrix that is SPD with no zeros
+    Return the test matrix
+    """
+    if size <= 1:
+        raise IndexError('unsupported size {} must be larger than 1'.format(size))
     
-    bMatrix = np.transpose(aMatrix)
-    testMatrix = aMatrix.dot(bMatrix)
+    testMatrix = None
+    aMatrix = np.random.randint(1,9,size=(size, size)) #generate random matrix with no zeros
+    bMatrix = np.transpose(aMatrix) #compute the transpose
+    testMatrix = aMatrix.dot(bMatrix) #generate an SPD matrix
     return testMatrix
 
 if __name__ == '__main__':
-    matrix = createTestMatrix(2)
-    print(matrix)
+    result = createTestMatrices(2, 12)
+    print(len(result))
